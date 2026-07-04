14d79267fef8d7449e67be8fb2706c2a85db8c7c
Rishabh Sharma <rishabhsharmx1@gmail.com>
2026-07-04 12:16:36 +0530

    Update Google Analytics Measurement ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -139,7 +139,7 @@ export default function RootLayout({
 
         {children}
 
-        <GoogleAnalytics gaId="G-2NG0NPXKF3" />
+        <GoogleAnalytics gaId="G-XWCQCWKX0L" />
       </body>
     </html>
   );
